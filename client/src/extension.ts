e1b8087c740aa2d65cba24dade4b80ce95ecbda1
Jad Ghalayini <jad.ghalayini@protonmail.com>
2025-10-16 20:20:04 +0200

    Linting

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -3,8 +3,8 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-import { ExtensionContext, Uri, window, workspace, commands } from 'vscode';
-import { LanguageClient, LanguageClientOptions, ServerOptions, RequestType } from 'vscode-languageclient/node';
+import { ExtensionContext, Uri, window, workspace } from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient/node';
 import { Wasm, ProcessOptions } from '@vscode/wasm-wasi/v1';
 import { createStdioOptions, createUriConverters, startServer } from '@vscode/wasm-wasi-lsp';
 

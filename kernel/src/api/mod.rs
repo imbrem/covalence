cfc8e0c3ebad1c4af762265773675dab7da22ec1
Jad Ghalayini <jad.ghalayini@protonmail.com>
2025-11-04 18:06:06 +0000

    Removed old visitor/rewrite code

--- kernel/src/api/mod.rs
+++ kernel/src/api/mod.rs
@@ -10,14 +10,8 @@ pub mod strategy;
 /// API for sound derivations
 pub mod derive;
 
-/// API for primitive rewriting rules supported by the kernel
-pub mod rewrite;
-
 /// API for errors
 pub mod error;
-
-/// API for visitors
-pub mod visit;
 
 /// API for universe management
 pub mod univ;

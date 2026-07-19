73a4c37a0321640079a318b5d12135124b54c7cd
Jad Ghalayini <jad@tekne.dev>
2026-07-19 01:28:15 +0100

    Add core computability capabilities

--- crates/lang/computation/src/lib.rs
+++ crates/lang/computation/src/lib.rs
@@ -24,6 +24,7 @@
 pub mod automata_theory;
 pub mod blc;
 pub mod compiler;
+pub mod computability;
 pub mod encoding;
 pub mod execution;
 pub mod finite;
@@ -38,6 +39,15 @@ pub use compiler::{
     CompilationArtifact, CompiledTerm, Compiler, CompilerLaws, CompilerReplayBackend,
     CompilerSearchWitness, ComposedArtifact, ComputationalEquivalence, DomainDecision,
     PartialCompilation, PartialCompiler, PartialCompilerLaws, SimulationVocabulary, StepMapping,
+};
+pub use computability::{
+    ComputableEquivalence, ComputablePredicate, ComputablePredicateFacts, ComputableRelation,
+    ComputableSet, DecidableSet, DecidableSetFacts, EnumerableSet, EnumerableSetFacts,
+    HostComputationWitness, HostPartialFunction, ManyOneReduction, ManyOneReductionFacts,
+    PartialCompilerComputabilityFacts, PartialCompilerTransport, PartialComposition,
+    PartialCompositionFacts, PartialComputable, PartialComputableFacts, PartialFunction,
+    RecognizableSet, RecognizableSetFacts, SymbolicComputability, SymbolicError, SymbolicLogic,
+    TotalComposition, TotalCompositionFacts, TotalComputable, TotalComputableFacts,
 };
 pub use encoding::{
     BitCodec, BitDecode, BitEncode, ByteCodec, ByteDecode, ByteEncode, DecodeError, Decoder,
